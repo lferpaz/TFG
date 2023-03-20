4f25b5c0c7bfe084b90fb775cc8362d865c62cd5
Luis Fernando <1567369@uab.cat>
2023-03-20 02:05:58 +0100

    Primera prueba de modelo

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-from utils import *
+import datetime
 import os
 import cv2
 import random
@@ -16,182 +16,167 @@ from skimage.metrics import structural_similarity as ssim
 from sklearn.model_selection import train_test_split
 
 
-def split_data(dir_path, categories, train_prop=0.7, image_size=(64, 64)):
-    # Obtener todas las imágenes auténticas y manipuladas
-    auth_images = glob.glob(os.path.join(dir_path, "Au", "*.jpg"))
-    manip_images = glob.glob(os.path.join(dir_path, "Tp", "*.jpg"))
+import tensorflow as tf
+from tensorflow.keras.layers import GlobalAveragePooling2D, Dense
+from tensorflow.keras.applications.resnet50 import ResNet50
+from tensorflow.keras.models import Model
+from tensorflow.keras.callbacks import TensorBoard
 
+
+def create_resnet50(input_shape):
+    # Cargar el modelo ResNet50 pre-entrenado sin la capa final
+    model = ResNet50(include_top=False, weights='imagenet', input_shape=input_shape)
+
+    # Congelar los pesos de todas las capas del modelo ResNet50
+    for layer in model.layers:
+        layer.trainable = False
+
+    # Agregar una capa de Global Average Pooling para reducir la dimensionalidad de las características
+    x = model.output
+    x = GlobalAveragePooling2D()(x)
+
+    # Agregar una capa totalmente conectada de 128 neuronas
+    x = Dense(128, activation='relu')(x)
+
+    # Agregar la capa de salida con una única neurona y función de activación sigmoidea
+    predictions = Dense(1, activation='sigmoid')(x)
+
+    # Combinar el modelo base con las nuevas capas
+    model = Model(inputs=model.input, outputs=predictions)
+
+    return model
+
+
+def load_and_preprocess_image(img_path, image_size):
+    img = tf.io.read_file(img_path)
+    img = tf.image.decode_jpeg(img, channels=3)
+    img = tf.image.resize(img, image_size)
+    img /= 255.0
+    return img
+
+def load_and_preprocess_mask(img_path, image_size):
+    img = tf.io.read_file(img_path)
+    img = tf.image.decode_jpeg(img, channels=3)
+    img = tf.image.resize(img, image_size)
+    img /= 255.0
+    img = tf.image.rgb_to_grayscale(img)
+    return img
+
+
+def split_data(dir_path, categories, train_prop=0.7, image_size=(64, 64), batch_size=32):
+    # Obtener todas las imágenes auténticas y manipuladas
+    auth_images = tf.data.Dataset.list_files(os.path.join(dir_path, "Au", "*.jpg"))
+    manip_images = tf.data.Dataset.list_files(os.path.join(dir_path, "Tp", "*.jpg"))
 
     # Seleccionar aleatoriamente imágenes para los conjuntos de entrenamiento y prueba
-    num_auth_train = round(len(auth_images) * train_prop)
-    num_manip_train = round(len(manip_images) * train_prop)
-    train_auth_indices = np.random.choice(len(auth_images), num_auth_train, replace=False)
-    train_manip_indices = np.random.choice(len(manip_images), num_manip_train, replace=False)
-    test_auth_indices = np.setdiff1d(np.arange(len(auth_images)), train_auth_indices)
-    test_manip_indices = np.setdiff1d(np.arange(len(manip_images)), train_manip_indices)
+    num_auth_images = len(list(auth_images))
+    num_manip_images = len(list(manip_images))
+    num_auth_train = round(num_auth_images * train_prop)
+    num_manip_train = round(num_manip_images * train_prop)
 
-    # Cargar solo las imágenes necesarias y redimensionarlas en paralelo
-    train_images = Parallel(n_jobs=multiprocessing.cpu_count())(delayed(resize_image)(auth_images[i], image_size) for i in train_auth_indices) + \
-                   Parallel(n_jobs=multiprocessing.cpu_count())(delayed(resize_image)(manip_images[i], image_size) for i in train_manip_indices)
-    
-    # Agregar las máscaras de segmentación a las imágenes manipuladas
-    manip_train_images = [manip_images[i] for i in train_manip_indices]
-    masks = Parallel(n_jobs=multiprocessing.cpu_count())(delayed(segment_image)(img_path) for img_path in manip_train_images)
-    train_images += masks
+    auth_images = auth_images.shuffle(buffer_size=num_auth_images, reshuffle_each_iteration=True)
+    manip_images = manip_images.shuffle(buffer_size=num_manip_images, reshuffle_each_iteration=True)
 
-    test_images = Parallel(n_jobs=multiprocessing.cpu_count())(delayed(resize_image)(auth_images[i], image_size) for i in test_auth_indices) + \
-                  Parallel(n_jobs=multiprocessing.cpu_count())(delayed(resize_image)(manip_images[i], image_size) for i in test_manip_indices)
-    
-    # Agregar las máscaras de segmentación a las imágenes manipuladas de prueba
-    manip_test_images = [manip_images[i] for i in test_manip_indices]
-    masks = Parallel(n_jobs=multiprocessing.cpu_count())(delayed(segment_image)(img_path) for img_path in manip_test_images)
-    test_images += masks
-   
-    # Crear etiquetas de entrenamiento y prueba
-    train_labels = np.concatenate([np.zeros(num_auth_train), np.ones(num_manip_train)])
-    test_labels = np.concatenate([np.zeros(len(auth_images) - num_auth_train), np.ones(len(manip_images) - num_manip_train)])
+    train_auth_images = auth_images.take(num_auth_train)
+    train_manip_images = manip_images.take(num_manip_train)
 
+    test_auth_images = auth_images.skip(num_auth_train)
+    test_manip_images = manip_images.skip(num_manip_train)
 
-    # Agregar ceros a test_labels para igualar el número de elementos con test_images, incluyendo las máscaras de segmentación a la parte de prueba
-    num_test_masks = len(masks)
-    test_labels = np.concatenate([test_labels, np.zeros(num_test_masks)])
+    # Cargar imágenes de entrenamiento
+    train_auth_images = train_auth_images.map(lambda x: load_and_preprocess_image(x, image_size))
+    train_manip_images = train_manip_images.map(lambda x: load_and_preprocess_image(x, image_size))
 
-    #agregar ceros a train_labels para igualar el número de elementos con train_images, incluyendo las máscaras de segmentación a la parte de entrenamiento
-    num_train_masks = len(train_images) - len(train_labels)
-    train_labels = np.concatenate([train_labels, np.zeros(num_train_masks)])
+    # Concatenar las imágenes auténticas y manipuladas
+    train_images = train_auth_images.concatenate(train_manip_images)
+    train_labels = tf.data.Dataset.from_tensor_slices(np.concatenate((np.zeros(num_auth_train), np.ones(num_manip_train))))
 
-    #Mostrar numero de elementos en cada clase de entrenamiento y prueba
-    print("Número de elementos de train_images: ", len(train_images))
-    print("Número de elementos de train_labels: ", len(train_labels))
-    print("Número de elementos de test_images: ", len(test_images))
-    print("Número de elementos de test_labels: ", len(test_labels))
-    
-    # Mezclar los datos de entrenamiento y prueba con scikit-learn
-    train_images, test_images, train_labels, test_labels = train_test_split(np.concatenate([train_images, test_images]), np.concatenate([train_labels, test_labels]), test_size=1-train_prop, shuffle=True)
+    # Mezclar los datos y crear lotes
+    train_data = tf.data.Dataset.zip((train_images, train_labels)).shuffle(buffer_size=num_auth_train + num_manip_train).batch(batch_size)
 
-    return train_images, train_labels, test_images, test_labels
+    # Cargar imágenes de prueba
+    test_auth_images = test_auth_images.map(lambda x: load_and_preprocess_image(x, image_size))
+    test_manip_images = test_manip_images.map(lambda x: load_and_preprocess_image(x, image_size))
+
+    # Concatenar las imágenes auténticas y manipuladas
+    test_images = test_auth_images.concatenate(test_manip_images)
+    test_labels = tf.data.Dataset.from_tensor_slices(np.concatenate((np.zeros(num_auth_images - num_auth_train), np.ones(num_manip_images - num_manip_train))))
+
+    # Mezclar los datos y crear lotes
+    test_data = tf.data.Dataset.zip((test_images, test_labels)).shuffle(buffer_size=num_auth_images + num_manip_images - num_auth_train - num_manip_train).batch(batch_size)
+
+    return train_data, test_data
 
 
-def resize_image(image_path, image_size):
-    image = cv2.imread(image_path, cv2.COLOR_BGR2RGB)
-    resized = cv2.resize(image, image_size, interpolation=cv2.INTER_AREA)
-    return resized
+def train_model(train_data, test_data, epochs, batch_size, model_save_path):
+    # Crear el modelo y compilarlo
+    model = create_resnet50()
+    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
-def segment_image(image_path):
-    # Leer imagen
-    image = cv2.imread(image_path)
+    # Crear una instancia del objeto TensorBoard
+    tensorboard = TensorBoard(log_dir="logs/{}".format(time.time()))
 
-    # Convertir a escala de grises
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    # Entrenar el modelo
+    history = model.fit(train_data, epochs=epochs, batch_size=batch_size, validation_data=test_data, callbacks=[tensorboard])
 
-    # Aplicar umbralización adaptativa
-    thresh = cv2.adaptiveThreshold(gray,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,11,2)
+    # Guardar el modelo
+    model.save(model_save_path)
 
-    # Aplicar erosión y dilatación para eliminar el ruido y unir los objetos
-    kernel = np.ones((3,3), np.uint8)
-    erosion = cv2.erode(thresh, kernel, iterations=1)
-    dilation = cv2.dilate(erosion, kernel, iterations=1)
-
-    # Aplicar detección de contornos
-    contours, _ = cv2.findContours(dilation, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
-    # Dibujar los contornos en una imagen en blanco
-    mask = np.zeros(image.shape[:2], dtype="uint8")
-    for contour in contours:
-        cv2.drawContours(mask, [contour], -1, 255, -1)
-
-    # Redimensionar la máscara a 64x64
-    resized_mask = cv2.resize(mask, (64, 64))
-
-    return resized_mask
-
-
-def load_image(image_path):
-    image = cv2.imread(image_path, cv2.COLOR_BGR2RGB)
-    return image
-
-def show_samples_images(dir_path, categories, sample_size=2):
-    # Visualizar algunas imágenes de cada categoría del dataset
-    sample_images = []
-    sample_labels = []
-    with ThreadPoolExecutor() as executor:
-        for cat in categories:
-            auth_image_path = glob.glob(os.path.join(dir_path, "Au", f"Au_{cat}_*.jpg"))[0]
-            manip_image_path = glob.glob(os.path.join(dir_path, "Tp", f"Tp_D_CRN_S_N_{cat}*"))[0]
-            image_paths = [auth_image_path, manip_image_path]
-            images = list(executor.map(load_image, image_paths))
-            sample_images.extend(images)
-            sample_labels.extend([categories[cat], "Imagen Manipulada"])
-    plt.figure(figsize=(15, 10))
-    for i in range(sample_size * 2):
-        plt.subplot(2, sample_size, i+1)
-        plt.imshow(sample_images[i])
-        plt.title(sample_labels[i])
-    plt.show()
+    return history
 
 
 if __name__ == "__main__":
-    # Iniciar el cronómetro
-    start_time = time.time()
+    # Definir los parámetros
+    dir_path = "data\CASIA2.0\CASIA2.0"
+    # Definir las categorías de autenticidad
+    categories = ["Au", "Tp"]
 
-    # Ruta del dataset
-    dir_path = os.path.join("data", "CASIA2.0", "CASIA2.0")
-
-    # Hacer un diccionario con las categorías
-    categories = {
-        "ani": "Animales",
-        "arc": "Arquitectura",
-        "art": "Arte",
-        "cha": "Personajes",
-        "ind": "Interior",
-        "nat": "Naturaleza",
-        "pla": "Plantas",
-        "txt": "Texturas"
-    }
-
-    # Patrones de archivo de imágenes auténticas y manipuladas
-    authentic_pattern = os.path.join(dir_path, "Au", "*.jpg")
-    manipulated_pattern = os.path.join(dir_path, "Tp", "*.jpg")
-
-    # Contar el número de imágenes auténticas y manipuladas
-    num_authentic = len(glob.glob(authentic_pattern))
-    num_manipulated = len(glob.glob(manipulated_pattern))
-
-    # Imprimir el número de imágenes auténticas y manipuladas
-    print(f"Número de imágenes auténticas: {num_authentic}")
-    print(f"Número de imágenes manipuladas: {num_manipulated}")
-
-    # Proporción de entrenamiento y prueba
+    # Definir la proporción de imágenes para el conjunto de entrenamiento
     train_prop = 0.7
 
-    # Tamaño de las imágenes
-    image_size = (224, 224)
+    # Definir el tamaño de las imágenes de entrada
+    image_size = (64, 64)
 
-    # Nombres de las clases
-    class_of_image = ["Au", "Tp"]
+    # Definir el tamaño del lote
+    batch_size = 32
 
-    # Dividir el dataset en conjuntos de entrenamiento y prueba
-    train_images, train_labels, test_images, test_labels = split_data(
-        dir_path,
-        class_of_image,
-        train_prop=train_prop,
-        image_size=image_size
-    )
+    # Definir el número de épocas
+    epochs = 10
 
-    # Mostrar algunas imágenes de cada categoría del dataset
-    show_samples_images(dir_path, categories)
+    #Cargar y dividir los datos
+    train_data, val_data = split_data(dir_path, categories, train_prop, image_size)
 
-    # Mostrar las imágenes de entrenamiento
-    plot_images(train_images, train_labels, (10, 10))
+    imput_shape = ()
+    #Definir el modelo
+    model = create_resnet50((64,64,3))
 
-    # Mostrar el tiempo de ejecución del programa
-    print(f"--- {time.time() - start_time} seconds ---")
+    #Compilar el modelo
+    model.compile(optimizer="adam", loss="binary_crossentropy", metrics=["accuracy"])
+
+    #Definir los callbacks
+    log_dir = "logs/fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+    tensorboard_callback = TensorBoard(log_dir=log_dir, histogram_freq=1)
+
+    #Entrenar el modelo 
+    history = model.fit(train_data,
+    epochs=epochs,
+    validation_data=val_data,
+    callbacks=[tensorboard_callback])
+
+    loss, acc = model.evaluate(val_data)
+
+    print("Loss: ", loss)
+    print("Accuracy: ", acc)
+
+    
+
+
+
+
+
+
 
 
 
     
-    
-
-    
-
 
