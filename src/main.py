01d11cb8d7e95e2f181a2067ae71c1b209106dab
Luis Fernando <1567369@uab.cat>
2023-03-25 00:42:47 +0100

    Primera aproximacion

--- src/main.py
+++ src/main.py
@@ -1,182 +1,102 @@
-import datetime
-import os
 import cv2
-import random
-import time 
-import glob
 import numpy as np
-import seaborn as sns
-import multiprocessing
-from pathlib import Path
-import matplotlib.pyplot as plt
-from sklearn.utils import shuffle
-from joblib import Parallel, delayed
-from concurrent.futures import ThreadPoolExecutor
-from skimage.metrics import structural_similarity as ssim
-from sklearn.model_selection import train_test_split
+import os
+from sklearn.svm import SVC
+from sklearn.model_selection import KFold
+from sklearn.metrics import accuracy_score
+from glob import glob
 
+# Función para preprocesar la imagen
+def preprocess_img(img):
+    img_resized = cv2.resize(img, (64, 64))
+    img_gray = cv2.cvtColor(img_resized, cv2.COLOR_BGR2GRAY)
+    if img_gray.dtype != np.float32:
+        img_gray = img_gray.astype(np.float32)
+    img_dct = cv2.dct(img_gray)
+    return img_dct
 
-import tensorflow as tf
-from tensorflow.keras.layers import GlobalAveragePooling2D, Dense
-from tensorflow.keras.applications.resnet50 import ResNet50
-from tensorflow.keras.models import Model
-from tensorflow.keras.callbacks import TensorBoard
-
-
-def create_resnet50(input_shape):
-    # Cargar el modelo ResNet50 pre-entrenado sin la capa final
-    model = ResNet50(include_top=False, weights='imagenet', input_shape=input_shape)
-
-    # Congelar los pesos de todas las capas del modelo ResNet50
-    for layer in model.layers:
-        layer.trainable = False
-
-    # Agregar una capa de Global Average Pooling para reducir la dimensionalidad de las características
-    x = model.output
-    x = GlobalAveragePooling2D()(x)
-
-    # Agregar una capa totalmente conectada de 128 neuronas
-    x = Dense(128, activation='relu')(x)
-
-    # Agregar la capa de salida con una única neurona y función de activación sigmoidea
-    predictions = Dense(1, activation='sigmoid')(x)
-
-    # Combinar el modelo base con las nuevas capas
-    model = Model(inputs=model.input, outputs=predictions)
-
-    return model
-
-
-def load_and_preprocess_image(img_path, image_size):
-    img = tf.io.read_file(img_path)
-    img = tf.image.decode_jpeg(img, channels=3)
-    img = tf.image.resize(img, image_size)
-    img /= 255.0
-    return img
-
-def load_and_preprocess_mask(img_path, image_size):
-    img = tf.io.read_file(img_path)
-    img = tf.image.decode_jpeg(img, channels=3)
-    img = tf.image.resize(img, image_size)
-    img /= 255.0
-    img = tf.image.rgb_to_grayscale(img)
-    return img
-
-
-def split_data(dir_path, categories, train_prop=0.7, image_size=(64, 64), batch_size=32):
-    # Obtener todas las imágenes auténticas y manipuladas
-    auth_images = tf.data.Dataset.list_files(os.path.join(dir_path, "Au", "*.jpg"))
-    manip_images = tf.data.Dataset.list_files(os.path.join(dir_path, "Tp", "*.jpg"))
-
-    # Seleccionar aleatoriamente imágenes para los conjuntos de entrenamiento y prueba
-    num_auth_images = len(list(auth_images))
-    num_manip_images = len(list(manip_images))
-    num_auth_train = round(num_auth_images * train_prop)
-    num_manip_train = round(num_manip_images * train_prop)
-
-    auth_images = auth_images.shuffle(buffer_size=num_auth_images, reshuffle_each_iteration=True)
-    manip_images = manip_images.shuffle(buffer_size=num_manip_images, reshuffle_each_iteration=True)
-
-    train_auth_images = auth_images.take(num_auth_train)
-    train_manip_images = manip_images.take(num_manip_train)
-
-    test_auth_images = auth_images.skip(num_auth_train)
-    test_manip_images = manip_images.skip(num_manip_train)
-
-    # Cargar imágenes de entrenamiento
-    train_auth_images = train_auth_images.map(lambda x: load_and_preprocess_image(x, image_size))
-    train_manip_images = train_manip_images.map(lambda x: load_and_preprocess_image(x, image_size))
-
-    # Concatenar las imágenes auténticas y manipuladas
-    train_images = train_auth_images.concatenate(train_manip_images)
-    train_labels = tf.data.Dataset.from_tensor_slices(np.concatenate((np.zeros(num_auth_train), np.ones(num_manip_train))))
-
-    # Mezclar los datos y crear lotes
-    train_data = tf.data.Dataset.zip((train_images, train_labels)).shuffle(buffer_size=num_auth_train + num_manip_train).batch(batch_size)
-
-    # Cargar imágenes de prueba
-    test_auth_images = test_auth_images.map(lambda x: load_and_preprocess_image(x, image_size))
-    test_manip_images = test_manip_images.map(lambda x: load_and_preprocess_image(x, image_size))
-
-    # Concatenar las imágenes auténticas y manipuladas
-    test_images = test_auth_images.concatenate(test_manip_images)
-    test_labels = tf.data.Dataset.from_tensor_slices(np.concatenate((np.zeros(num_auth_images - num_auth_train), np.ones(num_manip_images - num_manip_train))))
-
-    # Mezclar los datos y crear lotes
-    test_data = tf.data.Dataset.zip((test_images, test_labels)).shuffle(buffer_size=num_auth_images + num_manip_images - num_auth_train - num_manip_train).batch(batch_size)
-
-    return train_data, test_data
-
-
-def train_model(train_data, test_data, epochs, batch_size, model_save_path):
-    # Crear el modelo y compilarlo
-    model = create_resnet50()
-    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
-
-    # Crear una instancia del objeto TensorBoard
-    tensorboard = TensorBoard(log_dir="logs/{}".format(time.time()))
-
-    # Entrenar el modelo
-    history = model.fit(train_data, epochs=epochs, batch_size=batch_size, validation_data=test_data, callbacks=[tensorboard])
-
-    # Guardar el modelo
-    model.save(model_save_path)
-
-    return history
-
+# Función para extraer características de la imagen
+def extract_features(img):
+    if len(img.shape) == 3:
+        # Convert to grayscale
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    
+    
+    img = cv2.convertScaleAbs(img)
+    # Threshold using Otsu's method
+    cr = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+    # Extract features using Hu moments
+    moments = cv2.HuMoments(cv2.moments(cr)).flatten()
+    return moments
 
 if __name__ == "__main__":
-    # Definir los parámetros
-    dir_path = "data\CASIA2.0\CASIA2.0"
-    # Definir las categorías de autenticidad
-    categories = ["Au", "Tp"]
+    # Cargar las imágenes de entrenamiento y prueba
+    train_auth_path = "data/dataset/training/au/"
+    train_manip_path = "data/dataset/training/tampered/"
 
-    # Definir la proporción de imágenes para el conjunto de entrenamiento
-    train_prop = 0.7
-
-    # Definir el tamaño de las imágenes de entrada
-    image_size = (64, 64)
-
-    # Definir el tamaño del lote
-    batch_size = 32
-
-    # Definir el número de épocas
-    epochs = 10
-
-    #Cargar y dividir los datos
-    train_data, val_data = split_data(dir_path, categories, train_prop, image_size)
-
-    imput_shape = ()
-    #Definir el modelo
-    model = create_resnet50((64,64,3))
-
-    #Compilar el modelo
-    model.compile(optimizer="adam", loss="binary_crossentropy", metrics=["accuracy"])
-
-    #Definir los callbacks
-    log_dir = "logs/fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
-    tensorboard_callback = TensorBoard(log_dir=log_dir, histogram_freq=1)
-
-    #Entrenar el modelo 
-    history = model.fit(train_data,
-    epochs=epochs,
-    validation_data=val_data,
-    callbacks=[tensorboard_callback])
-
-    loss, acc = model.evaluate(val_data)
-
-    print("Loss: ", loss)
-    print("Accuracy: ", acc)
-
+    test_auth_path = "data/dataset/testing/au/"
+    test_manip_path = "data/dataset/testing/tampered/"
     
-
-
-
-
-
-
-
-
-
+    # Obtener las rutas de las imágenes de entrenamiento y prueba
+    train_paths = glob(train_auth_path + "*.png") + glob(train_manip_path + "*.tif")
+    test_paths = glob(test_auth_path + "*.png") + glob(test_manip_path + "*.tif")
     
+    # Cargar las imágenes de entrenamiento y prueba
+    train_imgs = [cv2.imread(img) for img in train_paths]
+    test_imgs = [cv2.imread(img) for img in test_paths]
+
+    # Preprocesar las imágenes de entrenamiento y prueba
+    train_imgs_preprocessed = [preprocess_img(img) for img in train_imgs]
+    test_imgs_preprocessed = [preprocess_img(img) for img in test_imgs]
+
+    # Extraer características de las imágenes de entrenamiento y prueba
+    train_features = np.array([extract_features(img) for img in train_imgs_preprocessed])
+    test_features = np.array([extract_features(img) for img in test_imgs_preprocessed])
+
+    # Definir las etiquetas de clasificación de las imágenes de entrenamiento y prueba
+    train_labels = np.array([0] * len(glob(train_auth_path + "*.png")) + [1] * len(glob(train_manip_path + "*.tif")))
+    test_labels = np.array([0] * len(glob(test_auth_path + "*.png")) + [1] * len(glob(test_manip_path + "*.tif")))
+
+
+    # Definir el modelo SVM con kernel RBF
+    svm = SVC(kernel='rbf')
+
+    # Realizar la validación cruzada k-fold para evaluar el rendimiento del modelo
+    kf = KFold(n_splits=10, shuffle=True, random_state=42)
+    accuracies = []
+    for train_index, test_index in kf.split(train_features):
+        X_train, X_test = train_features[train_index], train_features[test_index]
+        y_train, y_test = train_labels[train_index], train_labels[test_index]
+        svm.fit(X_train, y_train)
+        y_pred = svm.predict(X_test)
+        acc = accuracy_score(y_test, y_pred)
+        accuracies.append(acc)
+    print("Precisión promedio en validación cruzada: {:.2f}%".format(np.mean(accuracies)*100))
+
+    # Entrenar el modelo SVM con todos los datos de entrenamiento y evaluar en las imágenes de prueba
+    svm.fit(train_features, train_labels)
+    test_pred = svm.predict(test_features)
+
+    for i, img in enumerate(test_imgs):
+        # Obtener la etiqueta predicha para la imagen
+        label = test_pred[i]
+        # Asignar un texto a la imagen con la etiqueta predicha
+        if label == 0:
+            text = "Genuine"
+        if label == 1:
+            text = "Tampered"
+        cv2.putText(img, text, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+        # Mostrar la imagen
+        cv2.imshow("Image", img)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+        
+        
+
+
+
+
+
+
+
 
--- src/utils.py
+++ src/utils.py
@@ -4,29 +4,104 @@ import matplotlib.pyplot as plt
 import glob
 import os
 
-def resize_image(image_path, image_size):
-    # Cargar la imagen
-    image = cv2.imread(image_path, cv2.COLOR_BGR2RGB)
-    # Redimensionar la imagen
-    image = cv2.resize(image, image_size)
-    return image
+from skimage import color, util, io, img_as_float
+from scipy import fftpack
 
-def normalize_images(images):
-    normalized_images = []
+def read_folders(path):
+    '''
+    A partir del path que se nos pasa, leemos los diferentes folders que hay dentro y guardamos sus nombres en una lista
+    '''
+    folders = []
+    for folder in os.listdir(path):
+        folders.append(path + "/" + folder)
+    return folders
+
+def read_images(path):
+    '''
+    A partir del path que se nos pasa, guardamos las imagenes en una lista
+    '''
+    images = []
+    for image in glob.glob(path):
+        images.append(image)
+    return images
+    
+def read_images(path):
+    '''
+    A partir del path que se nos pasa, guardamos todas la imagenes en una lista
+    '''
+    
+    images = []
+    for image in glob.glob(path):
+        images.append(cv2.imread(image))
+    return images
+    
+
+
+def convert_imgs_to_YCrCb(images):
+    '''
+    Funcion para convertir una lista de imagenes a YCrCb
+    '''
+    images_YCrCb = []
     for image in images:
-        # Convertir la imagen a flotante y normalizar
-        normalized_image = cv2.normalize(image.astype('float'), None, 0.0, 1.0, cv2.NORM_MINMAX)
-        # Añadir la imagen normalizada a la lista
-        normalized_images.append(normalized_image)
-    return normalized_images
+        images_YCrCb.append(cv2.cvtColor(image, cv2.COLOR_BGR2YCrCb))
+    return images_YCrCb
 
-def plot_images(images,labels,size):
 
-    plt.figure(figsize=size)
-    for i in range(9):
-        plt.subplot(3, 3, i+1)
-        plt.imshow(images[i])
-        plt.title(f"Etiqueta: {labels[i]}")
-        plt.axis("off")
+
+def preprocess_images(images):
+    """
+    Realiza el preprocesamiento de una lista de imágenes.
+    """
+    preprocessed_images = []
+    for image in images:
+        # Convertir la imagen a YCbCr
+        ycbcr_image = color.rgb2ycbcr(image)
+        # Dividir la imagen en bloques de 8x8
+        blocks = util.view_as_blocks(ycbcr_image[:, :, 0], block_shape=(8, 8))
+        # Aplicar la transformada de coseno discreta en cada bloque
+        dct_blocks = fftpack.dct(fftpack.dct(blocks, axis=2, norm='ortho'), axis=3, norm='ortho')
+        # Agregar la imagen preprocesada a la lista
+        preprocessed_images.append(dct_blocks)
+    return preprocessed_images
+
+
+def plot_processed_images(processed_images, n, figsize=(20,20)):
+    """
+    Función para plotear n imágenes preprocesadas en el mismo plot.
+    """
+    plt.figure(figsize=figsize)
+    for i in range(n):
+        # Obtener el primer bloque de la imagen preprocesada y aplicar la inversa de la transformada de coseno discreta
+        # para reconstruir una imagen de 8x8 píxeles
+        block = processed_images[i][0, 0]
+        idct_block = fftpack.idct(fftpack.idct(block, axis=0, norm='ortho'), axis=1, norm='ortho')
+        # Escalar los valores de la imagen para que estén en el rango [0, 255]
+        scaled_block = (idct_block - np.min(idct_block)) * 255 / (np.max(idct_block) - np.min(idct_block))
+        # Crear una imagen RGB con el bloque escalado en el canal Y y ceros en los canales Cb y Cr
+        image = np.zeros((8, 8, 3), dtype=np.uint8)
+        image[:, :, 0] = scaled_block.astype(np.uint8)
+        # Mostrar la imagen
+        plt.subplot(1,n,i+1)
+        plt.imshow(image)
     plt.show()
 
+
+
+
+def plot_image(image):
+    '''
+    Funcion para plotear una imagen
+    '''
+    plt.imshow(image)
+    plt.show()
+
+
+def plot_images(images, n, figsize=(20,20)):
+    '''
+    Funcion para plotear n imagenes en el mismo plot
+    '''
+    fig, axs = plt.subplots(1, n, figsize=figsize)
+    for i in range(n):
+        axs[i].imshow(images[i])
+        axs[i].axis("off")
+    plt.show()
