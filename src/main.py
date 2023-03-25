cc1eacaa30c41f4990fc25198a5a139b3c72a4a6
Luis Fernando <1567369@uab.cat>
2023-03-25 20:59:59 +0100

    Agrego nuevo modelo

--- src/main.py
+++ src/main.py
@@ -1,102 +1,111 @@
 import cv2
 import numpy as np
-import os
+from sklearn.model_selection import train_test_split, KFold
 from sklearn.svm import SVC
-from sklearn.model_selection import KFold
 from sklearn.metrics import accuracy_score
-from glob import glob
+from keras.models import Sequential
+from keras.layers import Dense, Conv2D, MaxPooling2D, Flatten
+from keras.optimizers import Adam
+from keras.utils import to_categorical
+import os
+import glob
 
-# Función para preprocesar la imagen
-def preprocess_img(img):
-    img_resized = cv2.resize(img, (64, 64))
-    img_gray = cv2.cvtColor(img_resized, cv2.COLOR_BGR2GRAY)
-    if img_gray.dtype != np.float32:
-        img_gray = img_gray.astype(np.float32)
-    img_dct = cv2.dct(img_gray)
-    return img_dct
+# Cargar el conjunto de datos CASIA V2.0
+X = []
+y = []
 
-# Función para extraer características de la imagen
-def extract_features(img):
-    if len(img.shape) == 3:
-        # Convert to grayscale
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    
-    
-    img = cv2.convertScaleAbs(img)
-    # Threshold using Otsu's method
-    cr = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
-    # Extract features using Hu moments
-    moments = cv2.HuMoments(cv2.moments(cr)).flatten()
-    return moments
+# Cargar las imágenes auténticas y guardarlas en la lista X y la lista y con valor 0 limitado a 1000 imágenes
+for file_path in glob.glob('data/dataset/data/CASIA2/Au/*.jpg') [0:2000]:
+    img = cv2.imread(file_path)
+    if img is not None:
+        #antes de agregar la imagen la ponemos todas en el mismo tamaño
+        img = cv2.resize(img, (128, 128))
+        X.append(img)
+        y.append(0)
 
-if __name__ == "__main__":
-    # Cargar las imágenes de entrenamiento y prueba
-    train_auth_path = "data/dataset/training/au/"
-    train_manip_path = "data/dataset/training/tampered/"
+# Cargar las imágenes falsas y guardarlas en la lista X y la lista y con valor 1
+for file_path in glob.glob('data/dataset/data/CASIA2/Tp/*.jpg') [0:2000]:
+    img = cv2.imread(file_path)
+    if img is not None:
+        #antes de agregar la imagen la ponemos todas en el mismo tamaño
+        img = cv2.resize(img, (128, 128))
+        X.append(img)
+        y.append(1)
 
-    test_auth_path = "data/dataset/testing/au/"
-    test_manip_path = "data/dataset/testing/tampered/"
-    
-    # Obtener las rutas de las imágenes de entrenamiento y prueba
-    train_paths = glob(train_auth_path + "*.png") + glob(train_manip_path + "*.tif")
-    test_paths = glob(test_auth_path + "*.png") + glob(test_manip_path + "*.tif")
-    
-    # Cargar las imágenes de entrenamiento y prueba
-    train_imgs = [cv2.imread(img) for img in train_paths]
-    test_imgs = [cv2.imread(img) for img in test_paths]
+# Convertir las listas X e y en matrices numpy
+X = np.array(X, dtype=np.float32)
+y = np.array(y, dtype=np.int32)
 
-    # Preprocesar las imágenes de entrenamiento y prueba
-    train_imgs_preprocessed = [preprocess_img(img) for img in train_imgs]
-    test_imgs_preprocessed = [preprocess_img(img) for img in test_imgs]
+# Preprocesar las imágenes utilizando decorrelación y BDCT
+def preprocess(X):
+    X_gray = np.mean(X, axis=-1)
+    X_dct = cv2.dct(X_gray.astype(np.float32))
+    X_dct = np.expand_dims(X_dct, axis=-1)  # Agregar dimensión adicional
+    X_dct[:, :, 1:] *= 0.1
+    X_dct = cv2.idct(X_dct)
+    X_dct = cv2.normalize(X_dct, None, 0, 255, cv2.NORM_MINMAX).astype(np.uint8)
+    return X_dct
 
-    # Extraer características de las imágenes de entrenamiento y prueba
-    train_features = np.array([extract_features(img) for img in train_imgs_preprocessed])
-    test_features = np.array([extract_features(img) for img in test_imgs_preprocessed])
+# Aplicar el método mejorado de umbral
+def threshold(X):
+    _, X_thresh = cv2.threshold(X, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+    return X_thresh
 
-    # Definir las etiquetas de clasificación de las imágenes de entrenamiento y prueba
-    train_labels = np.array([0] * len(glob(train_auth_path + "*.png")) + [1] * len(glob(train_manip_path + "*.tif")))
-    test_labels = np.array([0] * len(glob(test_auth_path + "*.png")) + [1] * len(glob(test_manip_path + "*.tif")))
+# Separar los canales Cr, Y y Cb
+def split_channels(X):
+    if X.ndim == 2:
+        # Si la imagen es en escala de grises, convertirla a formato BGR
+        X = cv2.cvtColor(X, cv2.COLOR_GRAY2BGR)
+    elif X.ndim != 3 or X.shape[-1] != 3:
+        raise ValueError("La imagen debe estar en formato BGR")
 
+    # Convertir la imagen a formato YCrCb y separar los canales
+    X_yuv = cv2.cvtColor(X, cv2.COLOR_BGR2YUV)
+    X_cr, X_y, X_cb = cv2.split(X_yuv)
+    return X_cr, X_y, X_cb
 
-    # Definir el modelo SVM con kernel RBF
-    svm = SVC(kernel='rbf')
+# Utilizar las características resultantes de Cr, Y y Cb para la clasificación
+def extract_features(X):
+    X_cr, X_y, X_cb = split_channels(X)
+    X_features = np.stack([X_cr, X_y, X_cb], axis=-1)
+    return X_features
 
-    # Realizar la validación cruzada k-fold para evaluar el rendimiento del modelo
-    kf = KFold(n_splits=10, shuffle=True, random_state=42)
-    accuracies = []
-    for train_index, test_index in kf.split(train_features):
-        X_train, X_test = train_features[train_index], train_features[test_index]
-        y_train, y_test = train_labels[train_index], train_labels[test_index]
-        svm.fit(X_train, y_train)
-        y_pred = svm.predict(X_test)
-        acc = accuracy_score(y_test, y_pred)
-        accuracies.append(acc)
-    print("Precisión promedio en validación cruzada: {:.2f}%".format(np.mean(accuracies)*100))
+# Modelo CNN con kernel RBF y validación cruzada k-fold
+kf = KFold(n_splits=5)
+accuracies = []
 
-    # Entrenar el modelo SVM con todos los datos de entrenamiento y evaluar en las imágenes de prueba
-    svm.fit(train_features, train_labels)
-    test_pred = svm.predict(test_features)
+for train_idx, test_idx in kf.split(X):
+    X_train, X_test = X[train_idx], X[test_idx]
+    y_train, y_test = y[train_idx], y[test_idx]
 
-    for i, img in enumerate(test_imgs):
-        # Obtener la etiqueta predicha para la imagen
-        label = test_pred[i]
-        # Asignar un texto a la imagen con la etiqueta predicha
-        if label == 0:
-            text = "Genuine"
-        if label == 1:
-            text = "Tampered"
-        cv2.putText(img, text, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
-        # Mostrar la imagen
-        cv2.imshow("Image", img)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
-        
-        
+    print("Preprocesar y extraer características de las imágenes")
+    X_train_processed = np.array([extract_features(threshold(preprocess(img))) for img in X_train])
+    X_test_processed = np.array([extract_features(threshold(preprocess(img))) for img in X_test])
 
+    print("Convertir etiquetas a one-hot encoding")
+    y_train_onehot = to_categorical(y_train, num_classes=2)
+    y_test_onehot = to_categorical(y_test, num_classes=2)
 
+    print ("Crear modelo CNN")
+    model = Sequential()
+    model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(128, 128, 3)))
+    model.add(Conv2D(64, (3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Flatten())
+    model.add(Dense(128, activation='relu'))
+    model.add(Dense(2, activation='softmax'))
 
+    # Compilar modelo
+    optimizer = Adam(lr=0.0001)
+    model.compile(loss='categorical_crossentropy', optimizer=optimizer, metrics=['accuracy'])
 
+    # Entrenar modelo
+    model.fit(X_train_processed, y_train_onehot, epochs=10, batch_size=32, verbose=1)
 
+    # Evaluar modelo
+    y_pred = np.argmax(model.predict(X_test_processed), axis=-1)
+    accuracy = accuracy_score(y_test, y_pred)
+    accuracies.append(accuracy)
 
-
-
+#imprimir la precisión media
+print('Accuracy: ', np.mean(accuracies))
